ca1e63d53f8cceec1129dc6e279e147d1cdc22bc
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-08 18:42:15 +0900

    입출력 알고리즘 ( 1 ~ 3 )

---
+++ 2. inandout.js
@@ -0,0 +1,15 @@
+// 단계별로 풀기 1번 : 입출력
+
+// 1. Hello World 출력하기
+console.log('Hello World');
+
+// 2. We loce kriii
+// 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+
+for(var i = 1 ; i<=2 ; i++){
+    console.log('강한친구 대한육군');
+}
+
+// 3. 고양이
+// 고양이를 출력한다.
+console.log('\\    /\\\n )  ( \')\n(  /  )\n \\(__)|');
