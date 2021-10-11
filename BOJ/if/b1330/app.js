6c715d132dee320ce4f9e7e934569856974397c4
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-11 09:09:36 +0900

    백준 1330번 문제

--- BOJ/if/b1330/app.js
+++ BOJ/if/b1330/app.js
@@ -2,8 +2,20 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
-let inputArr ;
-
-for(var i = 0 ; i<2 ; i++){
-    inputArr = inputArr[i].split(' ');
+for(var i = 0 ; i<input.length ; i++){
+    solution(input[i].split(' ')[0], input[i].split(' ')[1])
 }
+
+function solution(a, b){
+    if(a>b){
+        console.log('>');
+    }
+    else if(a<b){
+        console.log('<');
+    }
+    else{
+        console.log('==');
+    }
+}
+
+
