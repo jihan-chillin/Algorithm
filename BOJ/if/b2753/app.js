59a10b8ad240ce0525ae48e3f1262baa23315389
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-12 10:06:29 +0900

    백준 2753문제 수정

--- BOJ/if/b2753/app.js
+++ BOJ/if/b2753/app.js
@@ -5,7 +5,7 @@ let input = fs.readFileSync(filePath).toString().split('\n');
 solution(+input[0]);
 
 function solution(a){
-    if(a%4 == 0 && a%100 != 100 || 400%a == 0){
+    if(a%4 == 0 && a%100 != 0 || a%400 == 0){
         console.log(1);
     }
     else{
