879d5ada2027df3fff8ced0fb572fd04fb902a2e
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-18 11:47:53 +0900

    백준 3052 문제

---
+++ BOJ/OneDemensionalArr/b3052/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n').map((item)=>+item);
+
+solution(input);
+
+function solution(input){
+    const count = [];
+    let num ; 
+
+    for(let i = 0 ; i<input.length ; i++){
+        num = input[i]%42;
+        if(count.indexOf(num) == -1 ){
+            count.push(num);
+        }
+    }
+    console.log(count.length);
+}
