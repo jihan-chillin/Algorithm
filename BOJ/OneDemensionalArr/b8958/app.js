35944425d684b72651b978866f71fc6bee5efca5
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-19 10:20:43 +0900

    백준 8958 문제

---
+++ BOJ/OneDemensionalArr/b8958/app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+solution(input);
+
+function solution(input){
+    const num = input[0];
+
+    for(let i = 1 ; i<=num ; i++){
+
+        let count = 0;
+        let sum = 0; 
+
+        for(let j = 0 ; j<input[i].length; j++){
+            if(input[i][j] === 'O'){
+                count ++;
+            }
+            else{
+                count = 0;
+            }
+            sum += count;
+        }
+        console.log(sum);
+    }
+    
+}
