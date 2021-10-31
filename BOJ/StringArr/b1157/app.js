8f816fba5fac3338032cb1c8eae371b7037ddc10
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-31 15:10:56 +0900

    백준 1157 문제 ( 다른 풀이방법 )

--- BOJ/StringArr/b1157/app.js
+++ BOJ/StringArr/b1157/app.js
@@ -6,22 +6,18 @@ solution(input);
 
 function solution(input){
     const result = new Array(26).fill(0);
-
-    for (let i = 0; i < input.length; i++) {
-    result[input.charCodeAt(i) - 97] ++;
+    for(let i = 0 ; i<26 ; i++){
+        result[input.charCodeAt(i) - 97] ++;
     }
-
+    
     const max = Math.max(...result);
     const index = result.indexOf(max);
 
-    let isSame = false;
-
-    for (let j = 0; j < 26; j++) {
-    if (result[j] === max && index != j) {
-        isSame = true;
-        break;
+    let count = 0;
+    for(let i = 0 ; i<26 ; i++){
+        if(result[i] === max){
+            count++;
+        }
     }
-    }
-
-    console.log(isSame ? "?" : String.fromCharCode(index + 65));
+    console.log(count > 1 ? '?' : String.fromCharCode(index +65));
 }
